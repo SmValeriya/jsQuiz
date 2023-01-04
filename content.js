61070ca03f59076dbac6cde1a5b8ee31cef27b11
SmValeriya <lerok1000@gmail.com>
2023-01-04 21:02:15 +0200

    Pug refactoring

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-export const data = {}
+export const data = {
+  "rules": [
+    "You will have only 15 seconds per each question.",
+    "After you check an answer, it can't be undone.",
+    "You can't select any option once time goes off.",
+    "You can't exit from the Quiz while you're playing.",
+    "You'll get points on the basis of your correct answers."
+  ]
+}
